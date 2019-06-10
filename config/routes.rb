35e46d3d897a2d332866776efed58e38e8836b0b
sachikovvv <believe.future@ymobile.ne.jp>
2019-06-10 18:54:13 +0900

    chek_inkurimental

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
     resources :messages, only: [:index, :create]
   end
 end
