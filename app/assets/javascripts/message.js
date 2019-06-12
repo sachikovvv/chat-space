48171ed4676904d5c77ec0d5fb965edc3440e5d0
sachikovvv <believe.future@ymobile.ne.jp>
2019-06-12 10:03:19 +0900

    create_auto_update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
-$(document).on('turbolinks:load', function(){
+$(function(){
+
   function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
+
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -13,7 +15,7 @@ $(document).on('turbolinks:load', function(){
                   </div>
                   <p class="lower-message">
                     <div>
-                    ${content}
+                    ${message.content}
                     </div>
                     ${img}
                   </p>
@@ -35,14 +37,40 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('#new_message')[0].reset();
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.form__submit').prop('disabled', false);
+      $('form')[0].reset();
     })
-    .fail(function(data){
+    .fail(function(){
       alert('エラーが発生したためメッセージは送信できませんでした。');
     })
-    .always(function(data){
-      $('.form__submit').prop('disabled', false);
-    })
   })
+  var reloadMessages = function () {
+    // if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+
+      var last_message_id = $('.message:last').data("message-id");
+      console.log(last_message_id)
+
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log(messages)
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        // alert("自動更新に失敗しました。");
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
