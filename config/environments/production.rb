854c5160776a01b91d6dd8fdf90427e5071b3e07
sachikovvv <believe.future@ymobile.ne.jp>
2019-06-15 00:54:05 +0900

    add_deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -81,6 +81,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  server '52.199.41.27', user: 'ec2-user', roles: %w{app db web}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
